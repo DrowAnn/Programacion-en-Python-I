38a287b48d243e338dd676b3734bca63f07cee89
Robert Steven Delgado Peralta <rs19990801@gmail.com>
2024-06-13 21:21:43 -0500

    Tercera Clase

---
+++ Clase-3.py
@@ -0,0 +1,28 @@
+# """
+# Captura 4 variables, imprimir de que tipo es cada una
+# """
+
+# num1 = int(input("Digite un numero: "))
+# num2 = float(input("Digite un numero decimal: "))
+# num3 = complex(input("Digite un numero complejo: "))
+# val1 = bool(input("Digite un valor booleano: "))
+# val2 = str(input("Digite un texto: "))
+
+# print(num1, "es de tipo ", type(num1))
+# print(num2, "es de tipo ", type(num2))
+# print(num3, "es de tipo ", type(num3))
+# print(val1, "es de tipo ", type(val1))
+# print(val2, "es de tipo ", type(val2))
+
+"""
+Suma de dos numeros
+"""
+
+name = input("Digita tu nombre: ")
+print(f"Hola, {name}, vamos a realizar una suma")
+
+num1 = int(input("Digite el primer numero: "))
+num2 = int(input("Digite el segundo numero: "))
+resultado = num1 + num2
+
+print(f"El resultado de la suma {num1} + {num2} es igual a {resultado}")
